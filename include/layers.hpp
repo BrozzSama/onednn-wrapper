58b0293c7edfef008161a0c7acece7a65f970d67
Francesco Brozzu <francesco.brozzu@eurecom.fr>
2021-04-12 02:32:48 -0700

    Cross entropy update

--- include/layers.hpp
+++ include/layers.hpp
@@ -6,6 +6,19 @@
 #include <random>
 #include "util.hpp"
 
+// Macro definition to recover Binary cross entropy loss layer in backward
+
+#define BCE_LOG 0
+#define BCE_INV 1
+#define BCE_LOG_INV 2
+#define BCE_REORDER 3
+#define BCE_REORDER_INV 4
+#define BCE_MATMUL 5
+#define BCE_MATMUL_INV 6
+#define BCE_SUM 7
+#define BCE_NORM 8
+
+
 using tag = dnnl::memory::format_tag;
 using dt = dnnl::memory::data_type;
 
@@ -364,6 +377,7 @@ int Dense_back(std::unordered_map<int, dnnl::memory> dense_fwd,
     return net.size() - 1;
 }
 
+/*
 int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true, 
                            std::vector<dnnl::primitive> &net,
                            std::vector<std::unordered_map<int, dnnl::memory>> &net_args,
@@ -374,7 +388,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     dnnl::memory::dims dim_nc_tt = {1, y_hat.get_desc().dims()[0]};
     dnnl::memory::dims dim_scalar = {1};
 
-    // Perform elementwise log on y_hat
+    // 1) Perform elementwise log on y_hat
     
     auto y_hat_log_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
     auto y_hat_log = memory(y_hat_log_md, eng);
@@ -387,7 +401,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     net_args.push_back({{DNNL_ARG_SRC, y_hat},
                         {DNNL_ARG_DST, y_hat_log}});
 
-    // Perform elementwise linear on y_hat with alpha = -1; beta = 1 ie. 1-y_hat
+    // 2) Perform elementwise linear on y_hat with alpha = -1; beta = 1 ie. 1-y_hat
 
     auto y_hat_inv_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
     auto y_hat_inv = memory(y_hat_inv_md, eng);
@@ -400,7 +414,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     net_args.push_back({{DNNL_ARG_SRC, y_hat},
                         {DNNL_ARG_DST, y_hat_inv}});
 
-    // Perform log on previously obtained element ie. log(1-y_hat)
+    // 3) Perform log on previously obtained element ie. log(1-y_hat)
 
     auto y_hat_inv_log_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
     auto y_hat_inv_log = memory(y_hat_inv_log_md, eng);
@@ -413,7 +427,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     net_args.push_back({{DNNL_ARG_SRC, y_hat_inv},
                         {DNNL_ARG_DST, y_hat_inv_log}});
 
-    // Reorder y_true from nc to cn {batch, 1} -> {1, batch}
+    // 4) Reorder y_true from nc to cn {batch, 1} -> {1, batch}
 
     auto y_true_reordered_md = dnnl::memory::desc(dim_nc_tt, dt::f32, tag::nc);
     auto y_true_reordered = memory(y_true_reordered_md, eng);
@@ -422,7 +436,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     net_args.push_back({{DNNL_ARG_FROM, y_true},
                             {DNNL_ARG_TO, y_true_reordered}});
 
-    // Perform elementwise linear on y_true_reordered with alpha = -1; beta = 1 ie. 1-y_true
+    // 5) Perform elementwise linear on y_true_reordered with alpha = -1; beta = 1 ie. 1-y_true
 
     auto y_true_inv_reordered_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
     auto y_true_inv_reordered = memory(y_true_inv_reordered_md, eng);
@@ -435,7 +449,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     net_args.push_back({{DNNL_ARG_SRC, y_true_reordered},
                         {DNNL_ARG_DST, y_true_inv_reordered}});
 
-    // Perform matmul(y_true_reordered, log(y_hat))
+    // 6) Perform matmul(y_true_reordered, log(y_hat))
 
     auto add1_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
     auto add1 = memory(add1_md, eng);
@@ -449,7 +463,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
                         {DNNL_ARG_DST, add1}});
 
 
-    // Perform matmul(1-y_true_reordered, log(1-y_hat))
+    // 7) Perform matmul(1-y_true_reordered, log(1-y_hat))
 
     auto add2_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
     auto add2 = memory(add2_md, eng);
@@ -462,7 +476,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
                         {DNNL_ARG_WEIGHTS, y_hat_inv_log_md},
                         {DNNL_ARG_DST, add2}});
 
-    // Sum obtained values (should be scalars)
+    // 8) Sum obtained values (should be scalars)
 
     auto loss_sum_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
     auto loss_sum = memory(loss_sum_md, eng);
@@ -485,7 +499,7 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
     net_args.push_back(sum_args);
     
 
-    // Divide by -N using elementwise linear
+    // 9) Divide by -N using elementwise linear
 
     auto loss_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
     auto loss = memory(loss_md, eng);
@@ -504,29 +518,82 @@ int binaryCrossEntropyLoss(dnnl::memory y_hat, dnnl::memory y_true,
 
 }
 
-int binaryCrossEntropyLoss_back(dnnl::memory y_hat, dnnl::memory y_true, 
+int binaryCrossEntropyLoss_back(std::vector<dnnl::primitive> net_fwd, 
+                           int start,
                            std::vector<dnnl::primitive> &net,
                            std::vector<std::unordered_map<int, dnnl::memory>> &net_args,
                            dnnl::engine eng)
 {
 
+    dnnl::memory loss = net_fwd[start+BCE_NORM][DNNL_ARG_DST];
+    dnnl::memory loss_sum = net_fwd[start+BCE_NORM][DNNL_ARG_SRC];
+
+    dnnl::memory y_true = net_fwd[start+BCE_REORDER][DNNL_ARG_FROM];
+    dnnl::memory y_true_reordered = net_fwd[start+BCE_REORDER][DNNL_ARG_TO];
+
+    dnnl::memory y_hat = net_fwd[start+BCE_LOG][DNNL_ARG_SRC];
+    dnnl::memory y_hat_log = net_fwd[start+BCE_LOG][DNNL_ARG_DST];
+
+    dnnl::memory 
+
     dnnl::memory::dims dim_nc = {y_hat.get_desc().dims()[0], 1};
     dnnl::memory::dims dim_nc_tt = {1, y_hat.get_desc().dims()[0]};
     dnnl::memory::dims dim_scalar = {1};
 
-    // Perform elementwise log on y_hat
+    // Backward flow starts from last element of forward flow
+
+    // Divide by -N using elementwise linear
+
+    auto loss_diff_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
+    auto loss_diff = memory(loss_md, eng);
+
+    float batch_size_inv = 1/y_true.dims()[0];
+
+    auto loss_desc = dnnl::eltwise_forward::desc(dnnl::prop_kind::forward_training, dnnl::algorithm::eltwise_linear,
+                                                y_true_reordered.get_desc(), -batch_size_inv, 0.f);
+    auto loss_pd = dnnl::eltwise_forward::primitive_desc(loss_desc, eng);
+
+    // Allocate memory
+
+    auto loss_sum_diff = memory(loss_sum.get_desc(), eng);
+
+    // Diff
+
+    auto loss_bwd_desc = dnnl::eltwise_backward::desc(dnnl::algorithm::eltwise_linear,
+                                                loss_diff_md, loss_sum.get_desc(), -batch_size_inv, 0.f);
+    auto loss_bwd_pd = dnnl::eltwise_backward::primitive_desc(loss_bwd_desc, eng,
+                                                                   loss_pd);
     
-    auto y_hat_log_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
-    auto y_hat_log = memory(y_hat_log_md, eng);
+    
+    net.push_back(dnnl::eltwise_backward(y_hat_log_pd));
+    net_args.push_back({{DNNL_ARG_SRC, loss_sum},
+                        {DNNL_ARG_DIFF_SRC, loss_sum_diff},
+                        {DNNL_ARG_DST, loss},
+                        {DNNL_ARG_DIFF_DST, loss_diff},
+                        });
 
-    auto y_hat_log_desc = dnnl::eltwise_forward::desc(dnnl::prop_kind::forward_training, dnnl::algorithm::eltwise_log,
-                                                y_hat.get_desc(), 0.f, 0.f);
-    auto y_hat_log_pd = dnnl::eltwise_forward::primitive_desc(y_hat_log_desc, eng);
+    // Sum obtained values (should be scalars)
 
-    net.push_back(dnnl::eltwise_forward(y_hat_log_pd));
-    net_args.push_back({{DNNL_ARG_SRC, y_hat},
-                        {DNNL_ARG_DST, y_hat_log}});
+    auto loss_sum_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
+    auto loss_sum = memory(loss_sum_md, eng);
 
+    std::vector<dnnl::memory::desc> add_vector_md = {add1_md, add2_md};
+    std::vector<dnnl::memory> add_vector = {add1, add2};
+    std::vector<std::float> scales = {1.f, 1.f};
+
+    auto loss_sum_pd = dnnl::sum::primitive_desc(scales, add_vector_md, eng);
+
+    net.push_back(dnnl::sum(loss_sum_pd));
+
+    std::unordered_map<int, memory> sum_args;
+
+    sum_args.insert({DNNL_ARG_DST, loss_sum});
+    for (int i = 0; i<add_vector.size(); i++){
+        sum_args.insert({DNN_ARG_SRC + i, add_vector[i]});
+    }
+
+    net_args.push_back(sum_args);
+    
     // Perform elementwise linear on y_hat with alpha = -1; beta = 1 ie. 1-y_hat
 
     auto y_hat_inv_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
@@ -602,47 +669,44 @@ int binaryCrossEntropyLoss_back(dnnl::memory y_hat, dnnl::memory y_true,
                         {DNNL_ARG_WEIGHTS, y_hat_inv_log_md},
                         {DNNL_ARG_DST, add2}});
 
-    // Sum obtained values (should be scalars)
-
-    auto loss_sum_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
-    auto loss_sum = memory(loss_sum_md, eng);
-
-    std::vector<dnnl::memory::desc> add_vector_md = {add1_md, add2_md};
-    std::vector<dnnl::memory> add_vector = {add1, add2};
-    std::vector<std::float> scales = {1.f, 1.f};
-
-    auto loss_sum_pd = dnnl::sum::primitive_desc(scales, add_vector_md, eng);
-
-    net.push_back(dnnl::sum(loss_sum_pd));
-
-    std::unordered_map<int, memory> sum_args;
-
-    sum_args.insert({DNNL_ARG_DST, loss_sum});
-    for (int i = 0; i<add_vector.size(); i++){
-        sum_args.insert({DNN_ARG_SRC + i, add_vector[i]});
-    }
-
-    net_args.push_back(sum_args);
     
 
-    // Divide by -N using elementwise linear
+    // If you are here you are done
+  
 
-    auto loss_md = dnnl::memory::desc(dim_scalar, dt::f32, tag::a);
-    auto loss = memory(loss_md, eng);
+    // Perform elementwise log on y_hat
+    
+    auto y_hat_log_diff_md = dnnl::memory::desc(dim_nc, dt::f32, tag::nc);
+    auto y_hat_log_diff = memory(y_hat_log_diff_md, eng);
 
-    float batch_size_inv = 1/y_true.dims()[0];
+    // Allocate memory
 
-    auto loss_desc = dnnl::eltwise_forward::desc(dnnl::prop_kind::forward_training, dnnl::algorithm::eltwise_linear,
-                                                y_true_reordered.get_desc(), -batch_size_inv, 0.f);
-    auto loss_pd = dnnl::eltwise_forward::primitive_desc(loss_desc, eng);
+    auto y_hat_diff = memory(y_hat.get_desc(), eng);
 
-    net.push_back(dnnl::eltwise_forward(loss_pd));
-    net_args.push_back({{DNNL_ARG_SRC, loss_sum},
-                        {DNNL_ARG_DST, loss}});   
+    // Recreate forward descriptor
+    auto y_hat_log_desc = dnnl::eltwise_forward::desc(dnnl::prop_kind::forward_training, dnnl::algorithm::eltwise_log,
+                                                y_hat.get_desc(), 0.f, 0.f);
+    auto y_hat_log_pd = dnnl::eltwise_forward::primitive_desc(y_hat_log_desc, eng);
+
+    // Diff
+
+    auto y_hat_log_bwd_desc = dnnl::eltwise_backward::desc(dnnl::algorithm::eltwise_log,
+                                                y_hat_log_diff_md, y_hat.get_desc(), 0.f, 0.f);
+    auto y_hat_log_bwd_pd = dnnl::eltwise_backward::primitive_desc(y_hat_log_bwd_desc, eng,
+                                                                   y_hat_log_pd);
+    
+    
+    net.push_back(dnnl::eltwise_backward(y_hat_log_pd));
+    net_args.push_back({{DNNL_ARG_SRC, y_hat},
+                        {DNNL_ARG_DIFF_SRC, y_hat_diff},
+                        {DNNL_ARG_DST, y_hat_log},
+                        {DNNL_ARG_DIFF_DST, y_hat_log_diff},
+                        });
+
     
     return net.size() - 1;
 
-}
+}*/
 
 /*
 
