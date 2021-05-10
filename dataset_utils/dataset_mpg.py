624a02518cb71029ae90404d102429ed66555e60
Francesco Brozzu <francesco.brozzu@eurecom.fr>
2021-05-10 01:12:31 -0700

    Update to dataset preparation functions

--- dataset_utils/dataset_mpg.py
+++ dataset_utils/dataset_mpg.py
@@ -3,25 +3,23 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import os
 
-data_path = "data/Admission_Predict.csv"
+data_path = "data/auto-mpg.data"
 
-df = pd.read_csv(data_path, delimiter=",")
+df = pd.read_csv(data_path, delimiter="\t")
 
 # NH format
 myScaler = MinMaxScaler()
 features = df.iloc[:, 1:-1].to_numpy(copy=True, dtype="float64")
-
 features = myScaler.fit_transform(features)
-
 label = df.iloc[:, -1].to_numpy(copy=True, dtype="float64").reshape([-1,1])
 
 shuffle_idx = np.random.permutation(range(features.shape[0]))
 
-features = features[shuffle_idx[0:6], :]
-label = label[shuffle_idx[0:6]]
+features = features[shuffle_idx, :]
+label = label[shuffle_idx]
 
 feature_file = open("data/features_admission.txt", "w")
-for row in features.T:
+for row in features:
     np.savetxt(feature_file, row, fmt="%f")
 
 feature_file.close()
