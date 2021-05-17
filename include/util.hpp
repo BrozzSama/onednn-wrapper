d26237d73cd0ee3d13ea246df7f1ff66f575cecb
volpe <volpe@eurecom.fr>
2021-05-17 01:22:43 -0700

    Commit per Francesco

--- include/util.hpp
+++ include/util.hpp
@@ -9,14 +9,14 @@ dnnl::memory checkType(dnnl::memory::desc md_true_type, dnnl::memory mem_to_chec
                        dnnl::engine eng)
 {
     auto mem_reordered = mem_to_check;
-    /*if (md_true_type != mem_to_check.get_desc())
+    if (md_true_type != mem_to_check.get_desc())
     {
         std::cout << "Memory mismatch adding reorder primitive\n";
         auto mem_reordered = dnnl::memory(md_true_type, eng);
         net.push_back(dnnl::reorder(mem_to_check, mem_reordered));
         net_args.push_back({{DNNL_ARG_FROM, mem_to_check},
                                 {DNNL_ARG_TO, mem_reordered}});
-    }*/
+    }
     return mem_reordered;
 }
 
