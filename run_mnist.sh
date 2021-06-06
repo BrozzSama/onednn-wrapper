abfcb69e1f4a23035beff679b8c52ee3f63577a2
Francesco Brozzu <francesco.brozzu@eurecom.fr>
2021-06-06 13:30:37 -0700

    MNIST config

--- run_mnist.sh
+++ run_mnist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source $ONEAPI_INSTALL/setvars.sh --force > /dev/null 2>&1
 echo "########## Executing the run"
-./dpcpp/onednn-training-mnist-cpp cpu config/config.json
+./dpcpp/onednn-training-mnist-cpp cpu config/config_mnist.json
 echo "########## Done with the run"
