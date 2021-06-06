a22e86b4b548b240039a167fa43796d3fd52c0fc
Francesco Brozzu <francesco.brozzu@eurecom.fr>
2021-06-06 13:27:47 -0700

    Added reorder primitive

--- include/primitive_wrappers.hpp
+++ include/primitive_wrappers.hpp
@@ -29,3 +29,16 @@ int Clip(dnnl::memory src_mem,
     return net.size() - 1;
 
 }
+
+int Reorder(dnnl::memory src_mem, dnnl::memory dst_mem,
+           std::vector<dnnl::primitive> &net,
+           std::vector<std::unordered_map<int, dnnl::memory>> &net_args,
+           dnnl::engine eng){
+    // Initialize output memory
+
+    net.push_back(dnnl::reorder(src_mem, dst_mem));
+    net_args.push_back({{DNNL_ARG_FROM, src_mem},
+                        {DNNL_ARG_TO, dst_mem}});
+
+    return net.size() - 1;
+}
