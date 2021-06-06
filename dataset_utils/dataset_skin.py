67260f7b5d4cb895e4ba95a4e4b92cfc52f30310
Francesco Brozzu <francesco.brozzu@eurecom.fr>
2021-06-06 13:28:16 -0700

    Use train_test split

--- dataset_utils/dataset_skin.py
+++ dataset_utils/dataset_skin.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-import os
+from sklearn.model_selection import train_test_split
 
 data_path = "data/SkinSegmentation.txt"
 
-#df = pd.read_csv(data_path, delimiter="\t")
-df = pd.read_csv(data_path, delimiter=",")
+df = pd.read_csv(data_path, delimiter="\t")
 
 # NH format
 myScaler = MinMaxScaler()
@@ -14,10 +13,7 @@ features = df.iloc[:, 0:-1].to_numpy(copy=True, dtype="float64")
 features = myScaler.fit_transform(features)
 label = df.iloc[:, -1].to_numpy(copy=True, dtype="float64").reshape([-1,1])
 
-shuffle_idx = np.random.permutation(range(features.shape[0]))
-
-features = features[shuffle_idx, :]
-label = label[shuffle_idx]
+features, features_val, labels, labels_val = train_test_split(features, label, test_size = 0.25, random_state = 42)
 
 feature_file = open("data/features_skin.txt", "w")
 for row in features:
@@ -26,12 +22,23 @@ for row in features:
 feature_file.close()
 
 label_file = open("data/label_skin.txt", "w")
-for row in label:
+for row in labels:
     np.savetxt(label_file, row, fmt="%f")
 
 label_file.close()
 
-np.save("data/features_skin.npy", features)
-np.save("data/features_label.npy", label)
+feature_file_val = open("data/features_skin_val.txt", "w")
+for row in features_val:
+    np.savetxt(feature_file_val, row, fmt="%f")
+
+feature_file_val.close()
+
+label_file_val = open("data/label_skin_val.txt", "w")
+for row in labels_val:
+    np.savetxt(label_file_val, row, fmt="%f")
+
+label_file_val.close()
+
+
 
 
