4c8007a09a1e1edcb88c84febda11ccbf83c83b8
Francesco Brozzu <francesco.brozzu@eurecom.fr>
2021-06-27 12:11:22 -0700

    Dynamic dataloader

--- src/misc/include/data_loader.h
+++ src/misc/include/data_loader.h
@@ -17,7 +17,7 @@ class DataLoader{
         std::vector<float> curr_batch, curr_batch_labels;
         void load_from_file(std::string filename, std::vector<float> &data);
         void write_to_memory(dnnl::memory dst_mem_features, dnnl::memory dst_mem_labels);
-        DataLoader(std::string features_path, std::string labels_path, int _dataset_size, int _minibatch_size, std::vector<long> dataset_shape, dnnl::engine _eng);
+        DataLoader(std::string features_path, std::string labels_path, int _minibatch_size, std::vector<long> dataset_shape, dnnl::engine _eng);
     private:
         int dataset_idx, sample_size;
         dnnl::engine eng;
--- src/misc/src/data_loader.cpp
+++ src/misc/src/data_loader.cpp
@@ -1,28 +1,27 @@
 #include "../include/data_loader.h"
 
-DataLoader::DataLoader(std::string features_path, std::string labels_path, int _dataset_size, int _minibatch_size, std::vector<long> dataset_shape, dnnl::engine _eng){
+DataLoader::DataLoader(std::string features_path, std::string labels_path, int _minibatch_size, std::vector<long> dataset_shape, dnnl::engine _eng){
     dataset_idx = 0;
-    dataset_size = _dataset_size;
-    minibatch_size = _minibatch_size;
     eng = _eng;
 
     // Load features
-    int total_size = std::accumulate(std::begin(dataset_shape), std::end(dataset_shape), 1, std::multiplies<int>());
-    dataset.reserve(total_size);
-    dataset.clear();
     load_from_file(features_path, dataset);
 
     // Load Labels
-    int n_samples = dataset_shape[0];
-    dataset_labels.reserve(n_samples);
-    dataset_labels.clear();
     load_from_file(labels_path, dataset_labels);
 
     // Prepare batch vector
-    sample_size = std::accumulate(std::begin(dataset_shape)+1, std::end(dataset_shape), 1, std::multiplies<int>()); 
+    sample_size = std::accumulate(std::begin(dataset_shape), std::end(dataset_shape), 1, std::multiplies<int>()); 
+    dataset_size = dataset.size()/sample_size;
+    std::cout << "Loaded a dataset of size: " << dataset_size;
+    if (_minibatch_size == -1){
+        minibatch_size = dataset_size;
+    }
+    else {
+        minibatch_size = _minibatch_size;
+    }
     int curr_batch_size = minibatch_size * sample_size;
     curr_batch.reserve(curr_batch_size);
-
     curr_batch_labels.reserve(minibatch_size);
 
 }
@@ -53,9 +52,12 @@ void DataLoader::load_from_file(std::string filename, std::vector<float> &data)
 {
     std::ifstream myfile(filename);
     int i = 0;
+    float curr;
     if (myfile.is_open())
     {
-        while (myfile >> data[i++]){}
+        while (myfile >> curr){
+            data.push_back(curr);
+        }
 
         myfile.close();
     }
